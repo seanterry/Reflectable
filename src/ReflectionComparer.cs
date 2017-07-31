3fc3968f6582c54c33582a7113d954ba59f04bb2
Sean Terry <sean.terry@seaseducation.com>
2017-07-31 07:52:02 -0500

    Reflection-based equality comparer

---
+++ src/ReflectionComparer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fidget.Extensions.Reflection
+{
+    /// <summary>
+    /// Compares the property values of instances of the type to determine equality.
+    /// </summary>
+    /// <typeparam name="T">Type of the objects being compared.</typeparam>
+
+    public class ReflectionComparer<T> : IEqualityComparer<T>
+    {
+        /// <summary>
+        /// Reflector for instances of the type.
+        /// </summary>
+        
+        static readonly ITypeReflector<T> Reflector = TypeReflectorExtensions.Reflect<T>();
+
+        /// <summary>
+        /// Private constructor.
+        /// </summary>
+        
+        ReflectionComparer() {}
+
+        /// <summary>
+        /// Gets an instance to compare the property values of instances of the type to determine equality.
+        /// </summary>
+        
+        public static IEqualityComparer<T> CompareProperties { get; } = new ReflectionComparer<T>();
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object of type T to compare.</param>
+        /// <param name="y">The second object of type T to compare.</param>
+
+        public bool Equals( T x, T y ) =>
+            x == null && y == null ? true :
+            x == null || y == null ? false :
+            Reflector.All( _=> _.Equal( x, y ) );
+
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <param name="obj">The object for which a hash code is to be returned.</param>
+        
+        public int GetHashCode( T obj )
+        {
+            if ( obj == null ) throw new ArgumentNullException( nameof( obj ) );
+
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ test/ReflectionComparerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Fidget.Extensions.Reflection
+{
+    public class ReflectionComparerTests
+    {
+        /// <summary>
+        /// Model type for comparison.
+        /// </summary>
+        
+        public class Model
+        {
+            public string Value { get; set; }
+        }
+
+        static string random() => Convert.ToBase64String( Guid.NewGuid().ToByteArray() );
+
+        IEqualityComparer<Model> instance => ReflectionComparer<Model>.CompareProperties;
+
+        public static IEnumerable<object[]> NotEqualCases = new object[][]
+        {
+            // one instance is null
+            new object[] { new Model { Value = random() }, null },
+            new object[] { null, new Model { Value = random() } },
+
+            // instances have different property values
+            new object[] { new Model { Value = random() }, new Model { Value = random() } },
+        };
+
+        [Theory]
+        [MemberData(nameof(NotEqualCases))]
+        public void Equals_whenNotEqual_returns_false( Model x, Model y )
+        {
+            var actual = instance.Equals( x, y );
+            Assert.False( actual );
+        }
+
+        public static IEnumerable<object[]> EqualCases()
+        {
+            var model = new Model { Value = random() };
+
+            // both instance are the same reference
+            yield return new object[] { model, model };
+
+            // both instances are null
+            yield return new object[] { null, null };
+
+            // both instances have the same property values
+            yield return new object[] { new Model { Value = model.Value }, new Model { Value = model.Value } };
+        }
+
+        [Theory]
+        [MemberData(nameof(EqualCases))]
+        public void Equals_whenEqual_returns_true( Model x, Model y )
+        {
+            var actual = instance.Equals( x, y );
+            Assert.True( actual );
+        }
+    }
+}
